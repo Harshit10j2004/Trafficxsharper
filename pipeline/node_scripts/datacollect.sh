1669f1f8b877333b02170f36c2530fbc5de0d694
harshit <onxy.harsh123@gmail.com>
2025-12-17 13:44:03 +0530

    new files and codes updated (ml and dashboard api started but no core feture implemented yet)

--- pipeline/node_scripts/datacollect.sh
+++ pipeline/node_scripts/datacollect.sh
@@ -2,16 +2,13 @@
 
 set -x
 
-data="/home/ubuntu/syslogs/sysscrlog/usage.log"
 rec_ip=""
 
 cpu=$(mpstat 1 1 | grep Average | awk '{print $12}')
 cpu_used_percent=$(echo "100 - $cpu" | bc)
-
 mem=$(free -m | grep Mem | awk '{print $2}')
 mem_used=$(free -m | grep Mem | awk '{print $3}')
 disk=$(df -h --total | grep total | awk '{print $5}' | tr -d '%')
-network_stats=$(ifstat -b 1 1 2>/dev/null | awk 'NR==3 {print $1, $2}')
 network_in_bytes=$(echo $network_stats | awk '{printf "%.0f", $1 * 1024}')
 network_out_bytes=$(echo $network_stats | awk '{printf "%.0f", $2 * 1024}')
 
