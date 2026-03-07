f1ed20d5480e32ed86b95660058c38bf807b8c77
harshit <onxy.harsh123@gmail.com>
2026-03-07 09:23:30 +0530

    multicloud support for the system added the azure cloud (untested)

--- deceng/decisioneng.py
+++ deceng/decisioneng.py
@@ -3,11 +3,17 @@ from pydantic import BaseModel
 from dotenv import load_dotenv
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
+from azure.mgmt.compute import ComputeManagementClient
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.resource import ResourceManagementClient
 import os
 import logging
 import requests
 import boto3
 import uuid
+import time
+import base64
 
 load_dotenv(r"/home/ubuntu/tsx/data/data.env")
 
@@ -69,13 +75,15 @@ class Metrics(BaseModel):
     client_id: str
     req_id: str
     joining_token: str
+    security_group: str
+
 
 class Scale_down(BaseModel):
-
     client_id: int
     instance_id: str
     node_id: str
     email: str
+
 
 ec2_client = None
 
@@ -87,14 +95,108 @@ def get_ec2():
     return ec2_client
 
 
-def get_elbv2():
-    global elbv2_client
-    if elbv2_client is None:
-        elbv2_client = boto3.client("elbv2", region_name="ap-south-1")
-    return elbv2_client
+def start_instance_azure(image, total_instances, server_type, pending_file, req_id, client_id, joining_token,
+                         security_group_id):
+    try:
+
+        userdata_template = """#!/bin/bash
+
+                LOCAL_IP=$(curl -s -H "Metadata:true" "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2021-02-01&format=text")
+
+                until systemctl is-active --quiet docker; do sleep 3; done
+
+                docker swarm join \\
+                  --token {joining_token} \\
+                  --advertise-addr "${{LOCAL_IP}}" \\
+                  --data-path-addr "${{LOCAL_IP}}" \\
+                  172.31.2.184:2377 || echo "Join failed" >&2
+                """
+
+        userdata = userdata_template.format(joining_token=joining_token)
+        custom_data = base64.b64encode(userdata.encode('utf-8')).decode('ascii')
+
+        subscription_id = os.getenv("SUB_ID")
+        resource_group = os.getenv("RES_GRP")
+        location = "eastus"
+
+        subnet_id = os.getenv("SUBNET_ID")
+
+        admin_username = os.getenv("AZURE_NAME")
+        admin_password = os.getenv("AZURE_PASS")
+        base_vm_name = "TSX-WORKER"
+
+        credential = DefaultAzureCredential()
+        compute_client = ComputeManagementClient(credential, subscription_id)
+        network_client = NetworkManagementClient(credential, subscription_id)
+
+        instance_ids = []
+
+        for i in range(1, total_instances + 1):
+            vm_name = f"{base_vm_name}-{client_id}-{i:03d}"
+
+            print(f"Creating VM: {vm_name}")
+
+            nic_name = f"{vm_name}-nic"
+            nic_poller = network_client.network_interfaces.begin_create_or_update(
+                resource_group,
+                nic_name,
+                {
+                    "location": location,
+                    "network_security_group": {"id": security_group_id},
+                    "ip_configurations": [{
+                        "name": "ipconfig1",
+                        "subnet": {"id": subnet_id},
+
+                    }]
+                }
+            )
+            nic = nic_poller.result()
+
+            vm_params = {
+                "location": location,
+                "hardware_profile": {"vm_size": server_type},
+                "storage_profile": {
+                    "image_reference": {"id": image},
+                    "os_disk": {
+                        "create_option": "FromImage",
+                        "managed_disk": {"storage_account_type": "Standard_LRS"}
+                    }
+                },
+                "os_profile": {
+                    "computer_name": vm_name,
+                    "admin_username": admin_username,
+                    "admin_password": admin_password,
+                    "custom_data": custom_data,
+
+                },
+                "network_profile": {
+                    "network_interfaces": [{"id": nic.id}]
+                }
+            }
+
+            vm_poller = compute_client.virtual_machines.begin_create_or_update(
+                resource_group, vm_name, vm_params
+            )
+            vm = vm_poller.result()
+
+            print(f"  → Created: {vm.name}")
+            instance_ids.append(vm.name)
+
+            os.makedirs(os.path.dirname(pending_file), exist_ok=True)
+            with open(pending_file, "a") as f:
+                for name in instance_ids:
+                    f.write(name + "\n")
+            time.sleep(2)
+
+        return instance_ids
+
+    except Exception:
+
+        logging.exception("Creating vm in azure caused issue",
+                          extra={"client_id": client_id, "req_id": req_id})
 
 
-def start_instance(ami, total_instances, server_type, pending_file, req_id, client_id, joining_token):
+def start_instance(ami, total_instances, server_type, pending_file, req_id, client_id, security_group, joining_token):
     try:
 
         userdata_template = """#!/bin/bash
@@ -110,8 +212,7 @@ def start_instance(ami, total_instances, server_type, pending_file, req_id, clie
           172.31.2.184:2377 || echo "Join failed" >&2
         """
 
-        #MANAGER_URL="http://13.233.139.228:9000/manager"
-        #dec_eng="http://13.233.117.62:8000/deceng_down"
+        security_group_main = [security_group]
 
         userdata = userdata_template.format(joining_token=joining_token)
 
@@ -121,14 +222,14 @@ def start_instance(ami, total_instances, server_type, pending_file, req_id, clie
             MaxCount=total_instances,
             InstanceType=server_type,
             KeyName=os.getenv("KEY"),
-            SecurityGroupIds=[os.getenv("SG")],
+            SecurityGroupIds=security_group_main,
             SubnetId=os.getenv("SUBID"),
             UserData=userdata,
             TagSpecifications=[
                 {
                     "ResourceType": "instance",
                     "Tags": [
-                        {"Key": "Name", "Value": "tsx-worker-1"},
+                        {"Key": "Name", "Value": f"tsx-worker-{client_id}"},
 
                     ]
                 }
@@ -179,17 +280,14 @@ def health_check(instance_id, req_id, client_id):
         raise
 
 
-def pop_next_instance(pending_file, req_id, client_id,instance_id):
-
+def pop_next_instance(pending_file, req_id, client_id, instance_id):
     if not os.path.exists(pending_file):
         logging.info(f"Pending file not found: {pending_file}",
                      extra={"Req_id": req_id, "client_id": client_id})
         return False
 
-
     with open(pending_file, "r") as f:
         all_ids = [line.strip() for line in f if line.strip()]
-
 
     original_count = len(all_ids)
     updated_ids = [iid for iid in all_ids if iid != instance_id]
@@ -222,11 +320,34 @@ def removing_instance(id, client_id, req_id):
 
     except Exception:
         logging.exception(
-            "Issue raised during terminating the instances",
+            "Issue raised during terminating the instances from aws",
             extra={"req_id": req_id, "client_id": client_id}
         )
 
-def mail(email,client_id,req_id,scale_message,total_instances):
+
+def removing_instance_azure(id, client_id, req_id):
+    try:
+
+        credential = DefaultAzureCredential()
+        subscription_id = os.getenv("SUB_ID")
+
+        resource_client = ResourceManagementClient(credential, subscription_id)
+
+        vm_resource_id = f"/subscriptions/{subscription_id}/resourceGroups/providers/Microsoft.Compute/virtualMachines/{id}"
+
+        poller = resource_client.resources.begin_delete_by_id(
+            resource_id=vm_resource_id
+        )
+
+        poller.wait()
+    except Exception:
+        logging.exception(
+            "Issue raised during terminating the vm from azure",
+            extra={"req_id": req_id, "client_id": client_id}
+        )
+
+
+def mail(email, client_id, req_id, scale_message, total_instances):
     try:
         payload = {
             "email": email,
@@ -249,7 +370,7 @@ def mail(email,client_id,req_id,scale_message,total_instances):
     except Exception:
 
         logging.exception("mailing api failed",
-        extra={"client_id": client_id, "req_id": req_id})
+                          extra={"client_id": client_id, "req_id": req_id})
 
         raise
 
@@ -264,12 +385,10 @@ def decengfunc(metrics: Metrics, bg: BackgroundTasks):
     client_id = metrics.client_id
     req_id = metrics.req_id
     joining_token = metrics.joining_token
+    security_group = metrics.security_group
 
     base_path = f"/home/ubuntu/tsx/data/instances/{client_id}"
     pending_file = f"{base_path}/pending.txt"
-    joined_file = f"{base_path}/joined.txt"
-
-    scale = None
 
     logging.info(
         "DECENG request received",
@@ -285,42 +404,56 @@ def decengfunc(metrics: Metrics, bg: BackgroundTasks):
 
     print(f"REQUEST ARRIVED {client_id} and generated requested id is {req_id} for server {scale_message}")
 
-
-
     try:
 
-            scale = "UP"
+        scale = "UP"
 
-            instance_id = start_instance(
-                ami,
-                total_instances,
-                server_type,
-                pending_file,
-                req_id,
-                client_id,
-                joining_token
-            )
+        instance_for_aws = int(total_instances/2)
+        instance_for_azure = total_instances - instance_for_aws
 
-            print(f"instance started {instance_id}")
+        instance_id = start_instance(
+            ami,
+            instance_for_aws,
+            server_type,
+            pending_file,
+            req_id,
+            client_id,
+            joining_token,
+            security_group
+        )
 
-            email = mail(email,client_id,scale,req_id,scale_message,total_instances)
+        instance_id_azure = start_instance_azure(
+            ami,
+            instance_for_azure,
+            server_type,
+            pending_file,
+            req_id,
+            client_id,
+            joining_token,
+            security_group
+        )
+
+        print(f"instance started {instance_id}")
+        print(f"instance started {instance_id_azure}")
+
+        email = mail(email, client_id, scale, req_id, scale_message, total_instances)
 
 
 
     except Exception:
-            logging.exception(
-                "AWS caused issue during scaling",
-                extra={"req_id": req_id, "client_id": client_id}
-            )
+        logging.exception(
+            "AWS caused issue during scaling",
+            extra={"req_id": req_id, "client_id": client_id}
+        )
 
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail="aws issue"
-            )
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="aws issue"
+        )
+
 
 @deceng.post("/deceng_down")
-def scale_down(metrics:Scale_down):
-
+def scale_down(metrics: Scale_down):
     scale_message = "DOWN"
     client_id = metrics.client_id
     instance_id = metrics.instance_id
@@ -333,24 +466,21 @@ def scale_down(metrics:Scale_down):
 
     try:
 
-            offboard = removing_instance(instance_id, req_id, client_id)
+        offboard = removing_instance(instance_id, req_id, client_id)
 
-            logging.info(f"The instances is deleted for client",
-                         extra={"req_id": req_id, "client_id": client_id, "instance_id": instance_id, "node_id": node_id})
+        logging.info(f"The instances is deleted for client",
+                     extra={"req_id": req_id, "client_id": client_id, "instance_id": instance_id, "node_id": node_id})
 
-            file_del = pop_next_instance(pending_file, req_id, client_id, instance_id)
+        file_del = pop_next_instance(pending_file, req_id, client_id, instance_id)
 
-            logging.info(f"The instances is cleared from record",
-                         extra={"req_id": req_id, "client_id": client_id, "instance_id": instance_id, "node_id": node_id})
+        logging.info(f"The instances is cleared from record",
+                     extra={"req_id": req_id, "client_id": client_id, "instance_id": instance_id, "node_id": node_id})
 
-
-            email = mail(email, client_id, req_id, scale_message, total_instances=1)
-
-
+        email = mail(email, client_id, req_id, scale_message, total_instances=1)
 
     except Exception:
 
-            logging.exception(
-                "AWS caused issue during offboard",
-                extra={"req_id": req_id, "client_id": client_id}
-            )
+        logging.exception(
+            "AWS caused issue during offboard",
+            extra={"req_id": req_id, "client_id": client_id}
+        )
