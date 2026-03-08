ede3de13004052251363f872600ae6187332573e
harshit <onxy.harsh123@gmail.com>
2026-03-08 09:25:37 +0530

    code fixed in deceng to handel both cloud

---
+++ system_scripts/instance_id_azure.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+HEADERS="Metadata: true"
+
+API_VERSION="2025-04-07"
+
+
+VM_ID=$(curl -s -H "$HEADERS" --noproxy "*" \
+    "http://169.254.169.254/metadata/instance/compute/vmId?api-version=$API_VERSION&format=text")
+
+if [ -z "$VM_ID" ]; then
+    VM_ID=$(curl -s -H "$HEADERS" --noproxy "*" \
+        "http://169.254.169.254/metadata/instance/compute?api-version=$API_VERSION" | \
+        grep -oP '(?<="vmId": ")[^"]+')
+fi
+
+NODE_ID=$(docker info --format '{{.Swarm.NodeID}}')
+
+echo "$VM_ID"   > /home/ubuntu/tsx/data/my-instance-id.txt
+echo "$NODE_ID" > /home/ubuntu/tsx/data/node_id.txt
+
