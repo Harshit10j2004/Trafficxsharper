5bef753c49d7129033d3f7ccda63f1e5b529b789
harshit <onxy.harsh123@gmail.com>
2026-05-21 23:56:51 +0530

    gmail architect got fixed

---
+++ alert/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Request
+from contextlib import asynccontextmanager
+import time
+from operations.email import router as mail
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+
+    print("App is starting up")
+    yield
+    print("App is shutting down")
+
+app = FastAPI(lifespan=lifespan)
+
+@app.middleware("http")
+async def middleware(request:Request,callnext):
+
+    starttime = time.time()
+    print(f"request path {request.url.path}")
+
+    response = await callnext(request)
+
+    process_time = time.time() - starttime
+    print(f"Completed in {process_time}")
+
+    return response
+
+
+
+app.include_router(mail)
+
+
+
+
+
---
+++ alert/operations/email.py
@@ -0,0 +1,92 @@
+from fastapi import APIRouter,HTTPException
+from pydantic import BaseModel
+import logging
+import smtplib
+from email.mime.text import MIMEText
+from alert.setting.loggers import LoggerFactory
+from alert.setting.conifg import settings
+
+
+logger = LoggerFactory.get_logger(
+    name="mail",
+    log_file=settings.LOG_FILE_MAIL,
+    level=logging.INFO
+)
+
+sender = settings.MAIL
+app_password = settings.PASSWORD
+
+class AlertData(BaseModel):
+
+    email: str
+    total_instances: int
+    scale: str
+    client_id: int
+    req_id:str
+
+router = APIRouter()
+
+@router.post("/email")
+async def mainmail(data:AlertData):
+
+
+    email = data.email
+    total_instance = data.total_instances
+    scale = data.scale
+    client_id = data.client_id
+    req_id = data.req_id
+
+
+    logger.info(
+        "alertapi request received",
+        extra={
+            "req_id": req_id,
+            "client_id": client_id,
+            "email": email,
+            "total_instance": total_instance,
+            "scale": scale
+        }
+    )
+
+    subject = f"TSX Alert: Infrastructure Scaled {scale}"
+    body = (f"Hello,This is an automated alert from TSX.Your infrastructure has been scaled {scale} due to observed or predicted load conditions crossing the configured threshold, total instances scaled {scale} are {total_instance}")
+
+    message = MIMEText(body, "plain")
+    message['Subject'] = subject
+    message['From'] = sender
+    message['To'] = email
+
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(sender, app_password)
+            server.sendmail(sender, email, message.as_string())
+
+        logger.info(
+            "alert email sent",
+            extra={
+                "req_id": req_id,
+                "client_id": client_id,
+                "total_instance": total_instance,
+                "scale": scale
+            }
+        )
+    except smtplib.SMTPAuthenticationError:
+        logging.exception("SMTP authentication failed",
+                          extra={"client_id": client_id, "req_id": req_id, "email": email}
+                          )
+        raise HTTPException(status_code=503, detail="email auth failed")
+
+    except smtplib.SMTPConnectError:
+        logging.exception("SMTP connection failed",
+                          extra={"client_id": client_id, "req_id": req_id, "email": email}
+                          )
+        raise HTTPException(status_code=503, detail="email server unreachable")
+
+    except Exception:
+        logging.exception("Unknown email error",
+                          extra={"client_id": client_id, "req_id": req_id, "email": email}
+                          )
+        raise HTTPException(status_code=503, detail="email service failed")
+
+
