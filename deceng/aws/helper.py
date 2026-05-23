d9c37d64fb39c55f1e9ca505eda0437d18372c91
harshit <onxy.harsh123@gmail.com>
2026-05-23 15:32:28 +0530

    added orm to broker and restructure the architect

--- deceng/aws/helper.py
+++ deceng/aws/helper.py
@@ -1,12 +1,28 @@
 import boto3
-
+import logging
+from deceng.setting.loggers import LoggerFactory
+from deceng.setting.conifg import settings
 ec2_client = None
+
+logger = LoggerFactory.get_logger(
+    name="aws_up",
+    log_file=settings.LOG_FILE_AWS_U,
+    level=logging.INFO
+)
 
 class get_ec2():
 
     @staticmethod
     async def get_ec2():
         global ec2_client
-        if ec2_client is None:
-            ec2_client = boto3.client("ec2", region_name="ap-south-1")
-        return ec2_client
+        try:
+            if ec2_client is None:
+                ec2_client = boto3.client("ec2", region_name="ap-south-1")
+            return ec2_client
+
+        except Exception as e:
+
+            logger.exception(
+                f"CODE caused issue during calling the helper function {e}",
+            )
+
